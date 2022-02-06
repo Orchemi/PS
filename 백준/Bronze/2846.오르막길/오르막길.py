5130e3d4bf3ec1517f81627bd24e58563e4fe89d
Orchemi <86189596+Orchemi@users.noreply.github.com>
2022-02-06 11:23:17 +0900

    [Bronze II] Title: 오르막길, Time: 68 ms, Memory: 30860 KB -BaekjoonHub

--- 백준/Bronze/2846.오르막길/오르막길.py
+++ 백준/Bronze/2846.오르막길/오르막길.py
@@ -1,21 +1,12 @@
-N = int(input())
-nums = list(map(int, input().split()))
-nums.append(0)
-
-val_low = nums[0]
-val_high = 0
-val_max = 0
-
-for i in range(1, N+1):
-    if nums[i] > nums[i-1]:
-        continue
+n = int(input())
+arr = list(map(int,input().split()))
+start,end = arr[0],arr[0]
+ans = 0
+for i in range(1,n):
+    if end >= arr[i]:
+        start = arr[i]
+        end = arr[i]
     else:
-        val_high = nums[i-1]
-
-        if val_max < val_high-val_low:
-            val_max = val_high-val_low
-
-        val_low = nums[i]
-        val_high = 0
-
-print(val_max)
+        end = arr[i]
+    ans = max(ans,end-start)
+print(ans)
