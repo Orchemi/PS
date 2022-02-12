77c78a53215f5a2e6146e1a9651bb774d0d52a0f
Orchemi <86189596+Orchemi@users.noreply.github.com>
2022-02-12 16:55:35 +0900

    [Bronze I] Title: 팰린드롬수, Time: 68 ms, Memory: 30860 KB -BaekjoonHub

--- 백준/Bronze/1259.팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259.팰린드롬수/팰린드롬수.py
@@ -1,13 +1,8 @@
-def func(n):
-    if len(n) < 2:
+while True:
+    N = input()
+    if N == '0':
+        quit()
+    elif N == N[::-1]:
         print('yes')
-    elif n[0] == n[-1]:
-        return func(n[1:-1])
     else:
         print('no')
-
-while True:
-    n = input()
-    if n == '0':
-        break
-    func(n)
