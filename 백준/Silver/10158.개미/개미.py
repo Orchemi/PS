c8e13f2dff2e696075b08598d2a5c59ae0b7211a
Orchemi <86189596+Orchemi@users.noreply.github.com>
2022-02-28 03:17:32 +0900

    [Silver IV] Title: 개미, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/10158.개미/개미.py
+++ 백준/Silver/10158.개미/개미.py
@@ -1,47 +1,23 @@
-import sys; input = sys.stdin.readline
+def func(T, n, L):
+    T = T % (2 * L)
+    if T == 0:
+        return n
+
+    if T >= L - n:
+        T -= (L - n)
+        n = L
+    else:
+        return n + T
+
+    if T >= L:
+        T -= L
+        return T
+    else:
+        return n - T
 
 I, J = map(int, input().split())
 ni, nj = map(int, input().split())
 di, dj = 1, 1
 ai, aj = -1, -1
-To = int(input())
-
-# 세로
-T = To % (2*I)
-if T==0:
-    ai = ni
-
-if ai==-1:
-    if T >= I-ni:
-        T -= (I-ni)
-        ni = I
-    else:
-        ai = ni + T
-
-if ai==-1:
-    if T >= I:
-        T -= I
-        ai = T
-    else:
-        ai = ni - T
-
-# 가로
-T = To % (2*J)
-if T==0:
-    aj = nj
-
-if aj==-1:
-    if T >= J-nj:
-        T -= (J-nj)
-        nj = J
-    else:
-        aj = nj + T
-
-if aj==-1:
-    if T >= J:
-        T -= J
-        aj = T
-    else:
-        aj = nj - T
-
-print(ai, aj)
+T = int(input())
+print(func(T, ni, I), func(T, nj, J))
