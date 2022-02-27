f77ae5dfcce73e167089966e272bb2e42394195d
Orchemi <86189596+Orchemi@users.noreply.github.com>
2022-02-28 03:22:06 +0900

    [Silver IV] Title: 개미, Time: 68 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Silver/10158.개미/개미.py
+++ 백준/Silver/10158.개미/개미.py
@@ -1,23 +1,18 @@
 def func(T, n, L):
     T = T % (2 * L)
-    if T == 0:
-        return n
+    if T == 0: return n
 
     if T >= L - n:
         T -= (L - n)
         n = L
-    else:
-        return n + T
+    else: return n + T
 
     if T >= L:
         T -= L
         return T
-    else:
-        return n - T
+    else: return n - T
 
 I, J = map(int, input().split())
 ni, nj = map(int, input().split())
-di, dj = 1, 1
-ai, aj = -1, -1
 T = int(input())
 print(func(T, ni, I), func(T, nj, J))
