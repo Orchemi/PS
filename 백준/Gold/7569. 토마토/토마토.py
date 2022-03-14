851472ef8782e3168c6eddc56e841215c0c2d009
Orchemi <86189596+Orchemi@users.noreply.github.com>
2022-03-14 23:44:01 +0900

    [Gold V] Title: 토마토, Time: 868 ms, Memory: 309604 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -19,12 +19,6 @@ for _ in range(H):
     mat = [list(map(int, input().split())) for _ in range(I)]
     cube.append(mat)
 
-# visited 정의
-visited = []
-for _ in range(H):
-    mat = [[0]*J for _ in range(I)]
-    visited.append(mat)
-
 # 저장될 때부터 모두 익으면(0이 없으면) 0 출력
 if not checkZero():
     print(0)
@@ -37,7 +31,6 @@ for h in range(H):
     for i in range(I):
         for j in range(J):
             if cube[h][i][j]==1:
-                visited[h][i][j] = 1
                 Q.append((0, h, i, j))
                 back += 1
 
@@ -52,19 +45,16 @@ while front < back-1:
     for dh, di, dj in dhij:
         # 탐색 방향이 상자 범위 이내이고
         if (0<=nh+dh<H) and (0<=ni+di<I) and (0<=nj+dj<J):
-            # 이전에 탐색하지 않았던 곳이며,
-            if not visited[nh+dh][ni+di][nj+dj]:
-                # 안 익은 토마토라면
-                if not cube[nh+dh][ni+di][nj+dj]:
-                    # visited와 cube에 탐색 위치를 체크
-                    cube[nh+dh][ni+di][nj+dj] = 1
-                    visited[nh+dh][ni+di][nj+dj] = 1
-                    # Q에 탐색 좌표와 일수를 추가
-                    Q.append((nday+1, nh+dh, ni+di, nj+dj))
-                    back += 1
+            # 안 익은 토마토라면
+            if not cube[nh+dh][ni+di][nj+dj]:
+                # cube에 탐색 위치를 체크
+                cube[nh+dh][ni+di][nj+dj] = 1
+                # Q에 탐색 좌표와 일수를 추가
+                Q.append((nday+1, nh+dh, ni+di, nj+dj))
+                back += 1
 
-                    if max_day < nday+1:
-                        max_day = nday+1
+                if max_day < nday+1:
+                    max_day = nday+1
 
 # 숙성 모두 마치고 안 익은 토마토가 있다면 -1 출력, 아니라면 max_day 출력
 print(-1 if checkZero() else max_day)
