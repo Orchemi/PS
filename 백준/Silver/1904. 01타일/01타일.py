e4782c419b64c4b1b12c7965df3dcc5fcd46087a
Orchemi <86189596+Orchemi@users.noreply.github.com>
2022-04-10 22:24:10 +0900

    [Silver III] Title: 01타일, Time: 120 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,11 +1,6 @@
 N = int(input())
-X = 3 if N<3 else N+1
-f = [0]*X
-f[0], f[1], f[2] = 1, 1, 2
-
-now = 3
-while now <= N:
-    f[now] = (f[now-1]+f[now-2]) % 15746
-    now += 1
-
-print(f[N] % 15746)
+i = a = b = 1
+while i < N:
+    a, b = b, (a+b)%15746
+    i += 1
+print(b)
