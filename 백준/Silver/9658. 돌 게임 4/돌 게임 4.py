3670444d72927e08a49f4381499bdba32f234404
Orchemi <86189596+Orchemi@users.noreply.github.com>
2022-04-19 00:24:52 +0900

    [Silver II] Title: 돌 게임 4, Time: 124 ms, Memory: 114408 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.py
@@ -0,0 +1,17 @@
+N = int(input())
+X = 10 if N<6 else N
+arr = [0]*(X+1)
+arr[1] = arr[3] = 1
+
+k = 5
+while k <= N:
+    f = 0
+    for i in (1, 3, 4):
+        if arr[k-i]:
+            f = 1
+            break
+    if not f:
+        arr[k] = 1
+    k += 1
+
+print('CY' if arr[N] else 'SK')
