f7d1f43351871990ce4729e155c02d858012ef0b
Orchemi <86189596+Orchemi@users.noreply.github.com>
2022-04-21 21:58:40 +0900

    [Gold IV] Title: 치즈, Time: 116 ms, Memory: 32476 KB -BaekjoonHub

---
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -0,0 +1,45 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+def SM(mat):
+    for l in mat:
+        print(*l)
+    print()
+
+
+def bfs(ni, nj):
+    global removeQ, space
+    Q = deque()
+    Q.append((ni, nj))
+    while Q:
+        ni, nj = Q.popleft()
+        if visit[ni][nj]: continue
+        visit[ni][nj] = 1
+
+        for di, dj in ((-1,0),(1,0),(0,1),(0,-1)):
+            si, sj = ni+di, nj+dj
+            if not (0<=si<I and 0<=sj<J): continue
+            if mat[si][sj]:
+                space[si][sj] += 1
+                if space[si][sj] == 2:
+                    removeQ.append((si, sj))
+            else:
+                Q.append((si, sj))
+
+
+I, J = map(int, input().split())
+mat = [list(map(int, input().split())) for _ in range(I)]
+space = [[0]*J for _ in range(I)]
+visit = [[0]*J for _ in range(I)]
+
+removeQ = deque()
+bfs(0, 0)
+time = 0
+while removeQ:
+    for _ in range(len(removeQ)):
+        ni, nj = removeQ.popleft()
+        bfs(ni, nj)
+    time += 1
+
+print(time)
