be552ea0e78fe4436660eeb4f8b301bcef7c34da
Orchemi <86189596+Orchemi@users.noreply.github.com>
2022-05-08 01:45:20 +0900

    [Gold III] Title: 나만 안되는 연애, Time: 148 ms, Memory: 31860 KB -BaekjoonHub

--- 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.py
+++ 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.py
@@ -1,6 +1,5 @@
 import sys
 input = sys.stdin.readline
-from heapq import heappop, heappush
 
 def find_x(x):
     if x!= p[x]:
@@ -13,12 +12,13 @@ p = [i for i in range(V+1)]
 Q = []
 for _ in range(E):
     u, v, w = map(int, input().split())
-    heappush(Q, (w, u, v))
+    Q.append((w, u, v))
 
+Q.sort(reverse=True)
 cnt = 0
 ssum = 0
 while cnt < V-1 and Q:
-    w, u, v = heappop(Q)
+    w, u, v = Q.pop()
     if G[u] == G[v]: continue
 
     a, b = find_x(u), find_x(v)
