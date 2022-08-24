7791d2b9e992235229d2147b4d853524e1c48ca3
Orchemi <86189596+Orchemi@users.noreply.github.com>
2022-08-24 22:34:23 +0900

    [Gold V] Title: 센서, Time: 6188 ms, Memory: 32440 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+N = int(input())
+K = int(input())
+arr = sorted(list(map(int, input().split())))
+
+dp = [0]*N
+for j in range(1, N):
+    dp[j] = dp[j-1] + arr[j] - arr[j-1]
+Q = deque()
+Q.append(dp)
+
+for i in range(1, K):
+    Q.append([0]*N)
+    for j in range(i+1, N):
+        Q[1][j] = min(Q[1][j-1] + arr[j] - arr[j-1], Q[0][j-1])
+    Q.popleft()
+
+print(Q[0][-1])
