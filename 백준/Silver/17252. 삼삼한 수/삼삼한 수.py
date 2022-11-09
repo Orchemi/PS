be4bcf73ac0108fcf99cfdb3b76bc5487b31b604
Orchemi <86189596+Orchemi@users.noreply.github.com>
2022-11-09 22:57:28 +0900

    [Silver III] Title: 삼삼한 수, Time: 1564 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/17252. 삼삼한 수/삼삼한 수.py
+++ 백준/Silver/17252. 삼삼한 수/삼삼한 수.py
@@ -2,9 +2,8 @@ def dfs(i, ssum):
     global N
     if ssum+(3**i) > N: return 0
     if ssum+(3**i) == N: return 1
-    if dfs(i+1, ssum): return 1
     if dfs(i+1, ssum+3**i): return 1
+    if dfs(i+1, ssum): return 1
 
-ret = False
 N = int(input())
 print('YES' if dfs(0, 0) else 'NO')
