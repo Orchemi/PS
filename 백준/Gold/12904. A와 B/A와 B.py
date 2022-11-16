abcf31609460021f7187b2bfd720c74e1750357c
Orchemi <86189596+Orchemi@users.noreply.github.com>
2022-11-17 01:19:10 +0900

    [Gold V] Title: A와 B, Time: 96 ms, Memory: 32440 KB -BaekjoonHub

--- 백준/Gold/12904. A와 B/A와 B.py
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -8,20 +8,10 @@ def main() :
     i, op = 0, -1
     while i < ql-sl:
         i += 1
-        if op==-1:
-            if Q[-1]=='A':
-                Q.pop()
-            else:
-                Q.pop()
-                op=D[op]
-
-        elif op==0:
-            if Q[0]=='A':
-                Q.popleft()
-            else:
-                Q.popleft()
-                op=D[op]
-
+        p = Q.pop() if op else Q.popleft()
+        if p == 'A': continue
+        op = D[op]
+        
     while Q:
         a = Q.pop() if op else Q.popleft()
         b = S.pop()
