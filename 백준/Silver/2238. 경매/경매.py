eb8816c845b33983b5d29907e993b960f560f48b
Orchemi <86189596+Orchemi@users.noreply.github.com>
2022-12-09 22:38:56 +0900

    [Silver V] Title: 경매, Time: 3976 ms, Memory: 38964 KB -BaekjoonHub

---
+++ 백준/Silver/2238. 경매/경매.py
@@ -0,0 +1,22 @@
+U, N = map(int, input().split())
+D = {}
+for _ in range(N):
+    name, price = input().split()
+    price = int(price)
+
+    if not D.get(price, []):
+        D[price] = []
+    D[price].append(name)
+
+min_cnt = 1e7
+min_price = 1e7
+for price, arr in D.items():
+    cnt = len(arr)
+    if min_cnt > cnt:
+        min_cnt = cnt
+        min_price = price
+
+    elif min_cnt == cnt:
+        min_price = min(min_price, price)
+
+print(D[min_price][0], min_price)
