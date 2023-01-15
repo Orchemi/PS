e5a8dcf2d2e452cadcfe7277620c22e058d8ae12
Orchemi <86189596+Orchemi@users.noreply.github.com>
2023-01-15 14:57:46 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 2292 ms, Memory: 73104 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
@@ -1,7 +1,11 @@
 import sys
 input = sys.stdin.readline
+from heapq import heappop, heappush
 
+Q = []
 N = int(input())
-arr = sorted([int(input()) for _ in range(N)], reverse=True)
-for a in arr:
-    print(a)
+for _ in range(N):
+    heappush(Q, -int(input()))
+
+while Q:
+    print(-heappop(Q))
