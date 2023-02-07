72a967d028066c834395f78866ac7a342f324883
Orchemi <86189596+Orchemi@users.noreply.github.com>
2023-02-07 22:41:19 +0900

    [Bronze II] Title: 자기복제수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2028. 자기복제수/자기복제수.py
+++ 백준/Bronze/2028. 자기복제수/자기복제수.py
@@ -1,9 +1,5 @@
 def main():
     N = int(input())
     M = N**2
-    Ns, Ms = str(N), str(M)
-    l = len(Ns)
-    return 'YES' if Ms[-l:] == Ns else 'NO'
-
-for _ in range(int(input())):
-    print(main())
+    print('YES' if str(M)[-len(str(N)):]==str(N) else 'NO')
+for _ in range(int(input())): main()
