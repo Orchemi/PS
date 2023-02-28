a9d7f4603d2fe54e288b463da0909b728aac88c0
Orchemi <86189596+Orchemi@users.noreply.github.com>
2023-02-28 23:53:15 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 1028 ms, Memory: 37164 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -0,0 +1,26 @@
+def main():
+    def make_memo():
+        memo = [[0]*J for _ in range(I)]
+        memo[0][0] = mat[0][0]
+        for j in range(1, J):
+            memo[0][j] = memo[0][j-1]+mat[0][j]
+        for i in range(1, I):
+            memo[i][0] = memo[i-1][0]+mat[i][0]
+        for i in range(1, I):
+            for j in range(1, J):
+                memo[i][j] = memo[i-1][j]+memo[i][j-1]-memo[i-1][j-1]+mat[i][j]
+        return memo
+
+
+    I, J = map(int, input().split())
+    mat = [list(map(int, input().split())) for _ in range(I)]
+    memo = make_memo()
+    T = int(input())
+    for _ in range(T):
+        i1, j1, i2, j2 = map(lambda x: int(x)-1, input().split())
+        a = memo[i1-1][j2] if i1 else 0
+        b = memo[i2][j1-1] if j1 else 0
+        c = 0 if not (i1 and j1) else memo[i1-1][j1-1]
+        print(memo[i2][j2]-a-b+c)
+
+main()
