5bc450f5bda29e873968ea818dbeb7941e978225
Orchemi <86189596+Orchemi@users.noreply.github.com>
2023-03-01 23:19:52 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 22240 ms, Memory: 83948 KB -BaekjoonHub

--- 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.py
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.py
@@ -2,11 +2,6 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-D = {}
-for _ in range(N):
-    n = int(input())
-    D[n] = D.get(n, 0)+1
-
-for k in sorted(D.keys()):
-    for _ in range(D[k]):
-        print(k)
+arr = sorted([int(input()) for _ in range(N)])
+for a in arr:
+    print(a)
