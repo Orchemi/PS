b72dfa263f5cc65d964d4533f8592e9af5bb691e
Orchemi <86189596+Orchemi@users.noreply.github.com>
2023-08-13 22:36:22 +0900

    [Silver II] Title: 내 뒤에 나와 다른 수, Time: 1276 ms, Memory: 212608 KB -BaekjoonHub

---
+++ 백준/Silver/24523. 내 뒤에 나와 다른 수/내 뒤에 나와 다른 수.py
@@ -0,0 +1,24 @@
+def main():
+    N = int(input())
+    arr = input().split()
+    cur = arr[0]
+    cnt = 1
+    i = 0
+    ret = []
+    while True:
+        if i+1 == N:
+            ret.append((-1, cnt))
+            break
+
+        if arr[i+1] == cur:
+            cnt += 1
+        else:
+            ret.append((i+2, cnt))
+            cur = arr[i+1]
+            cnt = 1
+        i += 1
+
+    for i, r in ret:
+        print(*[i]*r, end=' ')
+
+main()
