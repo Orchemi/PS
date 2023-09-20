1a9fa9717e961f1adc612c03b249373cadecec00
Orchemi <86189596+Orchemi@users.noreply.github.com>
2023-09-20 09:29:18 +0900

    [Silver III] Title: 수강신청, Time: 556 ms, Memory: 96116 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.py
+++ 백준/Silver/13414. 수강신청/수강신청.py
@@ -2,18 +2,14 @@ import sys
 
 N, M = map(int, sys.stdin.readline().rstrip().split())
 o1 = {}
-o2 = ['']
+o2 = {}
 for i in range(1, M+1):
     log = sys.stdin.readline().rstrip()
     pre_order = o1.get(log, 0)
     o1[log] = i
     if pre_order:
-        o2[pre_order] = ''
-    o2.append(log)
+        del o2[pre_order]
+    o2[i] = log
 
-cnt = 0
-for i in range(1, M+1):
-    if not o2[i]: continue
-    print(o2[i])
-    cnt += 1
-    if cnt == N: break
+for k in sorted(o2.keys())[:N]:
+    print(o2[k])
