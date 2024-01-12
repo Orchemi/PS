941b799132754f42eae7bd04ca0810fcb42c34b1
Orchemi <86189596+Orchemi@users.noreply.github.com>
2024-01-12 21:50:52 +0900

    [level Lv.0] Title: n보다 커질 때까지 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.js
@@ -0,0 +1,8 @@
+function solution(numbers, n) {
+    var answer = 0;
+    for (i=0; i<numbers.length; i++) {
+        if (answer > n) break;
+        answer += numbers[i]
+    }
+    return answer;
+}
